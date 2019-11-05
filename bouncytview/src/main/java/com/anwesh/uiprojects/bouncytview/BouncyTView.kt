daa2ced50af5ec7036a8ea7ad7f881afc4ddae65
Anwesh43 <anweshthecool0@gmail.com>
2019-11-05 10:52:41 +0530

    adding Animator class

--- bouncytview/src/main/java/com/anwesh/uiprojects/bouncytview/BouncyTView.kt
+++ bouncytview/src/main/java/com/anwesh/uiprojects/bouncytview/BouncyTView.kt
@@ -19,6 +19,7 @@ val sizeFactor : Float = 2.3f
 val hFactor : Float = 0.4f
 val foreColor : Int = Color.parseColor("#4CAF50")
 val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
@@ -88,6 +89,34 @@ class BouncyTView(ctx : Context) : View(ctx) {
             if (dir == 0f) {
                 dir = 1f - 2 * prevScale
                 cb()
+            }
+        }
+    }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(delay)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
             }
         }
     }
