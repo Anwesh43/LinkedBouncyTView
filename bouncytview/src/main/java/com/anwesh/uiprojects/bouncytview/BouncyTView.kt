86bd8447eacc3ffab3ad77f23ee23468f53aaed1
Anwesh43 <anweshthecool0@gmail.com>
2019-11-05 10:49:37 +0530

    adding BouncyTView class

--- bouncytview/src/main/java/com/anwesh/uiprojects/bouncytview/BouncyTView.kt
+++ bouncytview/src/main/java/com/anwesh/uiprojects/bouncytview/BouncyTView.kt
@@ -55,3 +55,20 @@ fun Canvas.drawBTNode(i : Int, scale : Float, paint : Paint) {
     restore()
 }
 
+class BouncyTView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
