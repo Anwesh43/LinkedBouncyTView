8e38868c1326c8e4bcea792e4623b211bf34b133
Anwesh43 <anweshthecool0@gmail.com>
2019-11-05 10:56:23 +0530

    adding BTNode class

--- bouncytview/src/main/java/com/anwesh/uiprojects/bouncytview/BouncyTView.kt
+++ bouncytview/src/main/java/com/anwesh/uiprojects/bouncytview/BouncyTView.kt
@@ -120,4 +120,46 @@ class BouncyTView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BTNode(var i : Int, val state : State = State()) {
+
+        private var next : BTNode? = null
+        private var prev : BTNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = BTNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBTNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BTNode {
+            var curr : BTNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
